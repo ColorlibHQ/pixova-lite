69d9817eb633a20b1c91f71194d632f9ec3a411a
Robciuc Ioan <robciucioan94@gmail.com>
2016-03-23 17:55:17 +0200

    Resolved #8.

--- inc/next-compatible.php
+++ inc/next-compatible.php
@@ -4,11 +4,11 @@
  */
 if( version_compare( $GLOBALS['wp_version'], '4.4.2', '>' ) ) {
 	// Add Image Size
-	add_image_size( 'pixova-lite-custom-logo', 141, 30 );
+	add_image_size( 'pixova-lite-custom-logo', 141, 30, true );
 
 	// Add Theme Support: Custom Logo
 	add_theme_support( 'custom-logo', array(
-		'sizes'	=> 'pixova-lite-custom-logo'
+		'size'	=> 'pixova-lite-custom-logo'
 	) );
 
 	// Logo
