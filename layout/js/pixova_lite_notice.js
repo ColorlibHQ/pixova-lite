4b0d410d991007a279aaef57c0ad5c080f739142
Giucu <giucu91@yahoo.com>
2017-06-27 12:47:03 +0300

    Jscs & Jshint

--- layout/js/pixova_lite_notice.js
+++ layout/js/pixova_lite_notice.js
@@ -1,11 +1,11 @@
-jQuery(document).on( 'click', '.pixova-error-update .notice-dismiss', function() {
+jQuery( document ).on( 'click', '.pixova-error-update .notice-dismiss', function() {// jscs:ignore validateLineBreaks
 
     jQuery.ajax({
         url: ajaxurl,
-        method: "POST",
+        method: 'POST',
         data: {
             action: 'pixova_lite_remove_upate_notice'
         }
-    })
+    });
 
-})
+});
