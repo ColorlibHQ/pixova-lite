fe5bd779cdfbc7c46dfecba0bf8f2d6a19f1ec7b
Giucu <giucu91@yahoo.com>
2017-07-07 12:56:08 +0300

    Fixed No results search page

--- search.php
+++ search.php
@@ -10,6 +10,8 @@
 						while ( have_posts() ) { the_post();
 							get_template_part( 'template-parts/content', get_post_format() );
 						}
+					}else{
+						get_template_part( 'template-parts/content', 'none' );
 					}
 					?>
 				</div><!--/.col-lg-8-->
