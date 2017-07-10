15a2358c88b80d1b8dc3853ac55bcd7f47803f44
Giucu <giucu91@yahoo.com>
2017-07-10 14:05:47 +0300

    Remove TGMPA & All of ThemeIsle recommended plugins #57

--- functions.php
+++ functions.php
@@ -78,11 +78,6 @@ if ( ! function_exists( 'pixova_lite_theme_setup' ) ) {
 		require get_template_directory() . '/widgets/class-pixova-lite-widget-social-media.php';
 
 		/**
-		 * PLugins
-		 */
-		require get_template_directory() . '/inc/plugin-activation.php';
-
-		/**
 		 * HTML Custom control
 		 */
 		require get_template_directory() . '/inc/customizer/class-pixova-lite-html-custom-control.php';
@@ -187,26 +182,12 @@ if ( ! function_exists( 'pixova_lite_theme_setup' ) ) {
 					'description' => esc_html__( 'If you just installed Pixova Lite, and are not able to see the one page template, you need to go to Settings -> Reading , Front page displays and select "Static Page".','pixova-lite' ),
 					'check' => pixova_lite_is_not_static_page(),
 				),
-				array(
-					'id' => 'pixova-lite-req-ac-install-pirate-forms',
-					'title' => esc_html__( 'Install Pirate Forms' ,'pixova-lite' ),
-					'description' => esc_html__( 'In the next updates, Pixova Lite\'s default contact form will be removed. Please make sure you install the Pirate Forms plugin to keep your site updated, and experience a smooth transition to the latest version.','pixova-lite' ),
-					'check' => defined( 'PIRATE_FORMS_VERSION' ),
-					'plugin_slug' => 'pirate-forms',
-				),
-				/*
                 array(
                     "id" => 'pixova-lite-req-ac-install-contact-forms',
                     "title" => esc_html__( 'Install Contact Form 7' ,'pixova-lite' ),
                     "description"=> esc_html__( 'In the next updates, Pixova Lite\'s default contact form will be removed. Please make sure you install the Pirate Forms plugin to keep your site updated, and experience a smooth transition to the latest version.','pixova-lite' ),
                     "plugin_slug" => 'contact-form-7'
                 ),
-                */
-				array(
-					'id' => 'pixova-lite-req-ac-check-pirate-forms',
-					'title' => esc_html__( 'Check the contact form after installing Pirate Forms' ,'pixova-lite' ),
-					'description' => esc_html__( 'After installing the Pirate Forms plugin, please make sure you check your frontpage contact form is working fine. Also, if you use Pixova Lite in other language(s) please make sure the translation is ok. If not, please translate the contact form again.','pixova-lite' ),
-				),
 			);
 
 			require get_template_directory() . '/inc/admin/welcome-screen/class-pixova-lite-welcome.php';
@@ -433,87 +414,6 @@ if ( ! function_exists( 'pixova_lite_comment_reply_js' ) ) {
 	}
 	add_action( 'comment_form_before', 'pixova_lite_comment_reply_js' );
 }
-
-
-if ( ! function_exists( 'pixova_lite_register_required_plugins' ) ) {
-	/**
-	 * Custom function to load TGMPA
-	 *
-	 * @since Pixova Lite 1.0.0
-	 */
-	function pixova_lite_register_required_plugins() {
-
-		/**
-		 * Array of plugin arrays. Required keys are name and slug.
-		 * If the source is NOT from the .org repo, then source is also required.
-		 */
-		$plugins = array(
-
-			array(
-				'name'                  => 'Pirate Forms - Contact Form and SMTP Plugin', // The plugin name.
-				'slug'                  => 'pirate-forms', // The plugin slug (typically the folder name).
-				'source'                => '', // The plugin source.
-				'required'              => false, // If false, the plugin is only 'recommended' instead of required.
-				'version'               => '1.0.4', // E.g. 1.0.0. If set, the active plugin must be this version or higher.
-				'force_activation'      => false, // If true, plugin is activated upon theme activation and cannot be deactivated until theme switch.
-				'force_deactivation'    => false, // If true, plugin is deactivated upon theme switch, useful for theme-specific plugins.
-				'external_url'          => '',// If set, overrides default API URL and points to an external URL.
-			),
-
-			array(
-				'name'                  => 'reCaptcha Security', // The plugin name.
-				'slug'                  => 'uber-nocaptcha-recaptcha', // The plugin slug (typically the folder name).
-				'source'                => '', // The plugin source.
-				'required'              => false, // If false, the plugin is only 'recommended' instead of required.
-				'version'               => '1.0.4', // E.g. 1.0.0. If set, the active plugin must be this version or higher.
-				'force_activation'      => false, // If true, plugin is activated upon theme activation and cannot be deactivated until theme switch.
-				'force_deactivation'    => false, // If true, plugin is deactivated upon theme switch, useful for theme-specific plugins.
-				'external_url'          => '',// If set, overrides default API URL and points to an external URL.
-			),
-		);
-
-		/**
-		 * Array of configuration settings. Amend each line as needed.
-		 * If you want the default strings to be available under your own theme domain,
-		 * leave the strings uncommented.
-		 * Some of the strings are added into a sprintf, so see the comments at the
-		 * end of each line for what each argument will be.
-		 */
-		$config = array(
-			'default_path' => '',                      // Default absolute path to pre-packaged plugins.
-			'menu' => 'mt-install-plugins', // Menu slug.
-			'has_notices' => true,                    // Show admin notices or not.
-			'dismissable' => true,                    // If false, a user cannot dismiss the nag message.
-			'dismiss_msg' => true,                      // If 'dismissable' is false, this message will be output at top of nag.
-			'is_automatic' => false,                   // Automatically activate plugins after installation or not.
-			'message' => '',                      // Message to output right before the plugins table.
-			'strings' => array(
-				'page_title' => __( 'Install Required Plugins', 'pixova-lite' ),
-				'menu_title' => __( 'Install Plugins', 'pixova-lite' ),
-				'installing' => __( 'Installing Plugin: %s', 'pixova-lite' ), // %s = plugin name.
-				'oops' => __( 'Something went wrong with the plugin API.', 'pixova-lite' ),
-				'notice_can_install_required' => _n_noop( 'This theme requires the following plugin: %1$s.', 'This theme requires the following plugins: %1$s.', 'pixova-lite' ), // %1$s = plugin name(s).
-				'notice_can_install_recommended' => _n_noop( 'This theme recommends the following plugin: %1$s.', 'This theme recommends the following plugins: %1$s.', 'pixova-lite' ), // %1$s = plugin name(s).
-				'notice_cannot_install' => _n_noop( 'Sorry, but you do not have the correct permissions to install the %s plugin. Contact the administrator of this site for help on getting the plugin installed.', 'Sorry, but you do not have the correct permissions to install the %s plugins. Contact the administrator of this site for help on getting the plugins installed.', 'pixova-lite' ), // %1$s = plugin name(s).
-				'notice_can_activate_required' => _n_noop( 'The following required plugin is currently inactive: %1$s.', 'The following required plugins are currently inactive: %1$s.', 'pixova-lite' ), // %1$s = plugin name(s).
-				'notice_can_activate_recommended' => _n_noop( 'The following recommended plugin is currently inactive: %1$s.', 'The following recommended plugins are currently inactive: %1$s.', 'pixova-lite' ), // %1$s = plugin name(s).
-				'notice_cannot_activate' => _n_noop( 'Sorry, but you do not have the correct permissions to activate the %s plugin. Contact the administrator of this site for help on getting the plugin activated.', 'Sorry, but you do not have the correct permissions to activate the %s plugins. Contact the administrator of this site for help on getting the plugins activated.', 'pixova-lite' ), // %1$s = plugin name(s).
-				'notice_ask_to_update' => _n_noop( 'The following plugin needs to be updated to its latest version to ensure maximum compatibility with this theme: %1$s.', 'The following plugins need to be updated to their latest version to ensure maximum compatibility with this theme: %1$s.', 'pixova-lite' ), // %1$s = plugin name(s).
-				'notice_cannot_update' => _n_noop( 'Sorry, but you do not have the correct permissions to update the %s plugin. Contact the administrator of this site for help on getting the plugin updated.', 'Sorry, but you do not have the correct permissions to update the %s plugins. Contact the administrator of this site for help on getting the plugins updated.', 'pixova-lite' ), // %1$s = plugin name(s).
-				'install_link' => _n_noop( 'Begin installing plugin', 'Begin installing plugins', 'pixova-lite' ),
-				'activate_link' => _n_noop( 'Begin activating plugin', 'Begin activating plugins', 'pixova-lite' ),
-				'return' => __( 'Return to Required Plugins Installer', 'pixova-lite' ),
-				'plugin_activated' => __( 'Plugin activated successfully.', 'pixova-lite' ),
-				'complete' => __( 'All plugins installed and activated successfully. %s', 'pixova-lite' ), // %s = dashboard link.
-				'nag_type' => 'updated',// Determines admin notice type - can only be 'updated', 'update-nag' or 'error'.
-			),
-		);
-
-		tgmpa( $plugins, $config );
-	}
-
-	add_action( 'tgmpa_register', 'pixova_lite_register_required_plugins' );
-}// End if().
 
 // Fallback nav menu
 if ( ! function_exists( 'pixova_lite_fallback_cb' ) ) {
